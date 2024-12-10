458eef1094a1ee67a47cab1aec60b22e5d0b0920
Jonas Päckos <jonas.packos@motorit.se>
2024-12-10 16:58:39 +0100

    Installed Storybook with basic setup

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+export default {
+  framework: {
+    name: "@storybook/html-webpack5",
+    options: {},
+  },
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.njk$/,
+      use: [
+        {
+          loader: "simple-nunjucks-loader",
+        },
+      ],
+    });
+    return config;
+  },
+};
---
+++ src/_includes/components/bettin/bettin.js
@@ -0,0 +1,18 @@
+import "./bettin.css";
+import renderButton from "./bettin.njk";
+
+/**
+ * @param {Object} args
+ * @param {string} args.label
+ * @param {boolean} [args.primary]
+ * @param {string} [args.size="medium"]
+ */
+export const createButton = (args = {}) => {
+  args.size = args.size || "medium";
+
+  // Setting a `storybookArgs` context variable causes the render() macro to
+  // be called automatically 📕✨
+  return renderButton({
+    storybookArgs: args,
+  });
+};
